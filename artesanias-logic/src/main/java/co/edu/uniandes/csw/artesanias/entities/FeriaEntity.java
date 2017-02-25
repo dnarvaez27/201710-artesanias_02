ddcc0e1c14ce7b2586a2beda52e13efcbab49810
ma.trujillo10 <ma.trujillo10@ISIS2603-0158.sis.virtual.uniandes.edu.co>
2017-02-24 20:17:52 -0500

    Entities Boleta, Ciudad, Espacio y Feria en proceso.

--- artesanias-logic/src/main/java/co/edu/uniandes/csw/artesanias/entities/FeriaEntity.java
+++ artesanias-logic/src/main/java/co/edu/uniandes/csw/artesanias/entities/FeriaEntity.java
@@ -13,20 +13,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
 
 /**
+ * Entidad de las ferias.
  * @author ma.trujillo10
  */
 @Entity
 public class FeriaEntity implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     /**
      * Id de la feria.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
@@ -37,22 +42,30 @@ public class FeriaEntity implements Serializable {
     /**
      * Fecha de inicio de la feria.
      */
+    @Temporal(javax.persistence.TemporalType.DATE)
     private Date inicio;
 
     /**
      * Fecha de fin de la feria.
      */
+    @Temporal(javax.persistence.TemporalType.DATE)
     private Date fin;
 
     /**
      * Espacio donde se da la feria.
      */
+    @ManyToOne(targetEntity = EspacioEntity.class)
+    @JoinColumn(name = "espacio_id")
     private EspacioEntity espacio;
 
     /**
      * Conjunto de artesanos que asistirán a la feria.
      */
     @ManyToMany
+    @JoinTable(
+      name="ferias_artesanos",
+      joinColumns=@JoinColumn(name="feria_id", referencedColumnName="id"),
+      inverseJoinColumns=@JoinColumn(name="artesano_id", referencedColumnName="id"))
     private Set<ArtesanoEntity> artesanos;
 
     /**
@@ -67,70 +80,80 @@ public class FeriaEntity implements Serializable {
     @OneToMany
     private Set<ConferenciaEntity> conferencias;
 
+    //--------------------------------------------------------------------------
+    // Métodos
+    //--------------------------------------------------------------------------
+
     public Long getId() {
         return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public Date getInicio() {
-        return inicio;
-    }
-
-    public Date getFin() {
-        return fin;
-    }
-
-    public EspacioEntity getEspacio() {
-        return espacio;
-    }
-
-    public Set<ArtesanoEntity> getArtesanos() {
-        return artesanos;
-    }
-
-    public Set<BoletaEntity> getBoletas() {
-        return boletas;
-    }
-
-    public Set<ConferenciaEntity> getConferencias() {
-        return conferencias;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Date getInicio() {
+        return inicio;
     }
 
     public void setInicio(Date inicio) {
         this.inicio = inicio;
     }
 
+    public Date getFin() {
+        return fin;
+    }
+
     public void setFin(Date fin) {
         this.fin = fin;
+    }
+
+    public EspacioEntity getEspacio() {
+        return espacio;
     }
 
     public void setEspacio(EspacioEntity espacio) {
         this.espacio = espacio;
     }
 
+    public Set<ArtesanoEntity> getArtesanos() {
+        return artesanos;
+    }
+
     public void setArtesanos(Set<ArtesanoEntity> artesanos) {
         this.artesanos = artesanos;
+    }
+
+    public Set<BoletaEntity> getBoletas() {
+        return boletas;
     }
 
     public void setBoletas(Set<BoletaEntity> boletas) {
         this.boletas = boletas;
     }
 
+    public Set<ConferenciaEntity> getConferencias() {
+        return conferencias;
+    }
+
     public void setConferencias(Set<ConferenciaEntity> conferencias) {
         this.conferencias = conferencias;
     }
 
+    
+
+    //--------------------------------------------------------------------------
+    // Métodos del objeto
+    //--------------------------------------------------------------------------
+    
     @Override
     public int hashCode() {
         int hash = 7;
@@ -144,7 +167,6 @@ public class FeriaEntity implements Serializable {
         if (obj == null)    return false;
         if (getClass() != obj.getClass())   return false;
         final FeriaEntity other = (FeriaEntity) obj;
-        if (!Objects.equals(this.id, other.id)) return false;
-        return true;
+        return Objects.equals(this.id, other.id);
     }
 }
