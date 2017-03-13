c3e0d1aafe04fe49bc68e7c91420272effcec60f
matrujillo10 <andrestrujillo098@gmail.com>
2017-03-13 16:39:27 -0500

    Correciones terminadas.

--- artesanias-02-web/src/main/java/co/edu/uniandes/csw/artesanias/dtos/detail/FeriaDetailDTO.java
+++ artesanias-02-web/src/main/java/co/edu/uniandes/csw/artesanias/dtos/detail/FeriaDetailDTO.java
@@ -1,9 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2017 Miller.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-
 import co.edu.uniandes.csw.artesanias.dtos.*;
 import co.edu.uniandes.csw.artesanias.entities.ArtesanoEntity;
 import co.edu.uniandes.csw.artesanias.entities.BoletaEntity;
@@ -24,7 +41,7 @@ public class FeriaDetailDTO extends FeriaDTO {
     
     private List<BoletaDTO> boletas;
     
-    //private List<ConferenciaDTO> conferencias;
+    private List<ConferenciaDTO> conferencias;
     
     public FeriaDetailDTO() {super();}
     
@@ -35,8 +52,8 @@ public class FeriaDetailDTO extends FeriaDTO {
             artesanos.add(new ArtesanoDTO(artesano));
         for (BoletaEntity boleta : entity.getBoletas())
             boletas.add(new BoletaDTO(boleta));
-        /*for (ConferenciaEntity conferencia : entity.getConferencias())
-            conferencias.add(new ConferenciaDTO(conferencia));*/
+        for (ConferenciaEntity conferencia : entity.getConferencias())
+            conferencias.add(new ConferenciaDTO(conferencia));
     }
 
     @Override
@@ -48,11 +65,10 @@ public class FeriaDetailDTO extends FeriaDTO {
         List<BoletaEntity> lbol = new LinkedList<BoletaEntity>();
         for (BoletaDTO boleta : boletas)
             lbol.add(boleta.toEntity());
-        /*List<ConferenciaEntity> lcon = new LinkedList<ConferenciaEntity>();
+        List<ConferenciaEntity> lcon = new LinkedList<ConferenciaEntity>();
         for (ConferenciaDTO conferencia : conferencias)
             lcon.add(conferencia.toEntity());
         entity.setConferencias(lcon);
-        */
         entity.setArtesanos(lart);
         entity.setBoletas(lbol);
         return entity;
@@ -74,12 +90,11 @@ public class FeriaDetailDTO extends FeriaDTO {
         this.boletas = boletas;
     }
     
-    /*public List<ConferenciaDTO> getConferencias() {
+    public List<ConferenciaDTO> getConferencias() {
         return conferencias;
     }
 
     public void setConferencias(List<ConferenciaDTO> conferencias) {
         this.conferencias = conferencias;
-    }*/
-    
+    }
 }
