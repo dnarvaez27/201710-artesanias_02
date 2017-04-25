932fd1612cfb0b083fbd503af7aace8bc206455c
ja.espinosa12 <ja.espinosa12@ISIS2603-0175.sis.virtual.uniandes.edu.co>
2017-04-25 12:27:18 -0500

    minor fixes

--- artesanias-02-logic/src/test/java/co/edu/uniandes/csw/artesanias/persistence/PabellonPersistenceTest.java
+++ artesanias-02-logic/src/test/java/co/edu/uniandes/csw/artesanias/persistence/PabellonPersistenceTest.java
@@ -6,13 +6,22 @@
 package co.edu.uniandes.csw.artesanias.persistence;
 
 import co.edu.uniandes.csw.artesanias.entities.PabellonEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
@@ -32,9 +41,28 @@ public class PabellonPersistenceTest {
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
+    @Inject
+    private PabellonPersistence pabellonPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<PabellonEntity> data = new ArrayList<PabellonEntity>();
     
     @Test
-    public void testSomeMethod() {
+    public void createPabellonTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        PabellonEntity newEntity = factory.manufacturePojo(PabellonEntity.class);
+
+        PabellonEntity result = pabellonPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+        PabellonEntity entity = em.find(PabellonEntity.class, result.getId());
+        Assert.assertNotNull(entity);
+        
     }
     
 }
