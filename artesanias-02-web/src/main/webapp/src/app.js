e9562bad244fd3d73dcfc4e6cbb451707a6bd1fa
Miller Andrés <andrestrujillo098@gmail.com>
2017-04-25 15:26:34 -0500

    Front end, active class.

--- artesanias-02-web/src/main/webapp/src/app.js
+++ artesanias-02-web/src/main/webapp/src/app.js
@@ -1,24 +1,29 @@
 (function (ng) {
-  var app = ng.module('mainApp', [
-    // External dependencies
-    'ui.router',
-    // Internal modules dependencies
-    'feriaModule',
-    'artesanoModule',
-    'espectadorModule',
-    'ciudadModule',
-    'organizadorModule',
-    'salonModule',
-    'conferenciaModule',
-    'espacioModule',
-    'boletaModule',
-    'dashboardModule'
-    
-  ]);
-  // Resuelve problemas de las promesas
-  app.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-  }]);
+    var app = ng.module('mainApp', [
+        // External dependencies
+        'ui.router',
+        // Internal modules dependencies
+        'feriaModule',
+        'artesanoModule',
+        'espectadorModule',
+        'ciudadModule',
+        'organizadorModule',
+        'salonModule',
+        'conferenciaModule',
+        'espacioModule',
+        'boletaModule',
+        'dashboardModule'
+
+    ]);
+    app.controller('mainController', function ($scope) {
+        $scope.isActive = function (path) {
+            return window.location.hash.substr(3, path.length) === path;
+        };
+    });
+    // Resuelve problemas de las promesas
+    app.config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }]);
 })(window.angular);
 
 
