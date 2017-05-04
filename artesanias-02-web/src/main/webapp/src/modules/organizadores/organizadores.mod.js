74e72032ba740b0691cc1f23aceb679c7a1fa0f9
ia.salazar <ia.salazar@ISIS2603-0194.sis.virtual.uniandes.edu.co>
2017-05-03 21:37:29 -0500

    minnor fixes

--- artesanias-02-web/src/main/webapp/src/modules/organizadores/organizadores.mod.js
+++ artesanias-02-web/src/main/webapp/src/modules/organizadores/organizadores.mod.js
@@ -3,86 +3,63 @@
     mod.constant("organizadorContext", "api/organizadores");
     mod.config(['$stateProvider',  '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/organizadores/';
-            var baseFeriaPath = 'src/modules/ferias/';
-            $urlRouterProvider.otherwise("/organizadoresList");
+            var baseFeriaPath = 'src/modules/feria/';
+            $urlRouterProvider.otherwise("/organizadores/List");
 
             $stateProvider.state('organizadores', {
                 url: '/organizadores',
                 abstract: true,
-                resolve: {
-                    organizadores: ['$http', 'organizadoresContext', function ($http, organizadoresContext) {
-                            return $http.get(organizadoresContext);
-                        }]
-                },
+                
                 views: {
                     'mainView': {
                         templateUrl: basePath + 'organizadores.html',
-                controller: ['$scope', 'organizadores', function ($scope, organizadores) {
-                                $scope.organizadoresRecords = organizadores.data;
-                            }]
+                
                        
                     }
                 }
             }).state('organizadoresList', {
                 url: '/list',
                 parent: 'organizadores',
-                views: {
-                    'listView': {
-                        templateUrl: basePath + 'organizadores.list.html',
-                        controller: ['$http', 'organizadoresContext',
-                function ($http, organizadoresContext) {
-
-                  function add (id, organizador) {
-                    $http.post(organizadoresContext + '/' + id, organizador);
-                  }
-
-                  function del (id) {
-                    $http.delete(organizadoresContext + '/' + id);
-                  }
-
-                  //TODO
-                  function crea () {
-                    var name = document.getElementById('nombre').innerHTML;
-                    var ide = document.getElementById('identificacion').innerHTML;
-
-                    var nuevoArtesano = {
-                      nombre: name,
-                      identificacion: ide
-                    };
-
-                    $http.post(organizadoresContext, nuevoArtesano);
-
-                  }
-                }]
-            }
-          }
-        }).state('organizadorDetail', {
-                url: '/{organizadorId:int}/detail',
-                parent: 'organizadoresList',
-                param: {
-                    organizadorId: null
-                },
-                
-                resolve:  {
-                    currentOrganizador: ['$http', 'organizadoresContext', '$stateParams', function ($http, organizadoresContext, $params) {
-                            return $http.get(organizadoresContext+'/'+$params.organizadorId);
+                resolve: {
+                    organizadores: ['$http', 'organizadorContext', function ($http, organizadorContext) {
+                            return $http.get(organizadorContext);
                         }]
                 },
                 views: {
-                    'detailView': {
-                        templateUrl: basePath + 'organizadores.detail.html',
-                        controller: ['$scope', 'currentOrganizador', function ($scope,  currentOrganizador) {
-                                $scope.currentOrganizador = currentOrganizador.data;
-                            }]
-                    },
                     'listView': {
-                        templateUrl: baseFeriaPath + 'feira.list.html',
-                        controller: ['$scope', 'currentOrganizador', function ($scope, currentOrganizador) {
-                                $scope.currentFeria = currentOrganizador.data.ferias;
+                        templateUrl: basePath + 'organizadores.list.html',
+                        controller: ['$scope', 'organizadores', function ($scope, organizadores) {
+                                $scope.organizadoresRecords = organizadores.data;
                             }]
-                    }
-                }
-            });
+            }
+          }})
+//        }).state('organizadorDetail', {
+//                url: '/{organizadorId:int}/detail',
+//                parent: 'organizadoresList',
+//                param: {
+//                    organizadorId: null
+//                },
+//                
+//                resolve:  {
+//                    currentOrganizador: ['$http', 'organizadorContext', '$stateParams', function ($http, organizadorContext, $params) {
+//                            return $http.get(organizadorContext+'/'+$params.organizadorId);
+//                        }]
+//                },
+//                views: {
+//                    'detailView': {
+//                        templateUrl: basePath + 'organizadores.detail.html',
+//                        controller: ['$scope', 'currentOrganizador', function ($scope,  currentOrganizador) {
+//                                $scope.currentOrganizador = currentOrganizador.data;
+//                            }]
+//                    },
+//                    'listView': {
+//                        templateUrl: baseFeriaPath + 'feira.list.html',
+//                        controller: ['$scope', 'currentOrganizador', function ($scope, currentOrganizador) {
+//                                $scope.currentFeria = currentOrganizador.data.ferias;
+//                            }]
+//                    }
+//                }
+//            });
         }]);
 
               
