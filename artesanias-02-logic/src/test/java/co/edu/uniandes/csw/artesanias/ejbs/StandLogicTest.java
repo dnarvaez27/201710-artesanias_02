16dddb5ab485fa8ae3bf424289e0c9e2b99e1c1e
ja.espinosa12 <ja.espinosa12@ISIS2603-0175.sis.virtual.uniandes.edu.co>
2017-05-10 19:32:18 -0500

    Test lógica y podamCiudad

---
+++ artesanias-02-logic/src/test/java/co/edu/uniandes/csw/artesanias/ejbs/StandLogicTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.artesanias.ejbs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ja.espinosa12
+ */
+public class StandLogicTest {
+    
+    public StandLogicTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
